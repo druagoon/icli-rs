d65b3939aecc4dc0832858dae95278a35f5cc154
lazyboy <lazyboy.fan@gmail.com>
2024-07-16 13:07:46 +0800

    feat(macros): add `include_template` and `string` macros

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod cli;
 mod commands;
 mod consts;
+mod macros;
 mod prelude;
 mod traits;
 
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,1 +1,4 @@
+#[allow(unused_imports)]
 pub use crate::traits::{CliCommand, CliCommandResult};
+#[allow(unused_imports)]
+pub use crate::{include_template, string};
