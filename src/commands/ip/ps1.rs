96d9e37521e3e608b6014dd288b2855d7cdfba2a
lazyboy <lazyboy.fan@gmail.com>
2024-07-28 11:10:41 +0800

    feat(ip): add ps1 --no-name

--- src/commands/ip/ps1.rs
+++ src/commands/ip/ps1.rs
@@ -4,21 +4,21 @@ use crate::prelude::*;
 
 /// Print network interface and ip part of the terminal shell's `$PS1`.
 #[derive(clap::Parser, Debug)]
-pub struct IpPs1Cmd {}
+pub struct IpPs1Cmd {
+    /// Don't show the network interface name.
+    #[arg(long)]
+    no_name: bool,
+}
 
 impl CliCommand for IpPs1Cmd {
     fn run(&self) -> CliResult {
         let mut outputs = vec![];
         let ifaces = interfaces();
-        for v in &ifaces {
-            if v.is_loopback() || v.ips.is_empty() {
-                continue;
-            }
-            for ip in &v.ips {
-                if ip.is_ipv4() {
-                    outputs.push(format!("{}={}", v.name, ip.ip()));
-                }
-            }
+        for v in ifaces.iter().filter(|&x| !(x.is_loopback() || x.ips.is_empty())) {
+            let inner = v.ips.iter().filter(|&x| x.is_ipv4()).map(|x| {
+                if self.no_name { x.ip().to_string() } else { format!("{}={}", v.name, x.ip()) }
+            });
+            outputs.extend(inner);
         }
         println!("{}", outputs.join(" "));
         Ok(())
